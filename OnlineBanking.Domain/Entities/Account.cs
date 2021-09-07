6acd365b2872544dc8665b2cada4d5878d91af34
Slimcent <33299753+Slimcent@users.noreply.github.com>
2021-09-07 16:32:25 +0100

    still on

--- OnlineBanking.Domain/Entities/Account.cs
+++ OnlineBanking.Domain/Entities/Account.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Text;
 using OnlineBanking.Domain.Enumerators;
 using OnlineBanking.Domain.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineBanking.Domain.Entities
 {
    public class Account: IEntity
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
 
         public int? UserId { get; set; }
 
         public int CustomerId { get; set; }
 
-        public string AccountNumber { get; set; }
+        [MinLength(10)]
+        [MaxLength(10)]
+        public int AccountNumber { get; set; }
 
+        [Column(TypeName = "decimal(38,2)")]
         public decimal Balance { get; set; }
 
         public AccountType AccountType { get; set; }
--- OnlineBanking.Domain/Entities/DataInitializer.cs
+++ OnlineBanking.Domain/Entities/DataInitializer.cs
@@ -14,25 +14,56 @@ namespace OnlineBanking.Domain.Entities
                  new Customer
                  {
                      Id = 1,
-                     
+                     FirstName = "Dara",
+                     LastName = "Success",
+                     Email = "dara@domain.com",
+                     Birthday = new DateTime(2000, 06, 21),
+                     Gender = Enumerators.Gender.Female
                  },
 
-                 new QuotesViewModel
+                 new Customer
                  {
                      Id = 2,
-                     Quote = "Just flow with the moment",
-                     Author = "Annonymous",
-                 },
-
-                 new QuotesViewModel
-                 {
-                     Id = 3,
-                     Quote = "The end will justify the beginning",
-                     Author = "Annonymous",
-                 },
+                     FirstName = "Obinna",
+                     LastName = "Achara",
+                     Email = "obinna@gmail.com",
+                     Birthday = new DateTime(2004, 10, 12),
+                     Gender = Enumerators.Gender.Male
+                 }
 
             };
+
+
+            var accounts = new List<Account>
+            {
+                new Account
+                {
+                    Id = 1,
+                    CustomerId = 1,
+                    AccountNumber = 0211979756,
+                    Balance =  23_456_782_340,
+                    AccountType =Enumerators.AccountType.Saving,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    CreatedBy = "Dara",
+                    UpdatedBy = "Dara"
+                },
+
+                new Account
+                {
+                    Id = 2,
+                    CustomerId = 2,
+                    AccountNumber = 0317092802,
+                    Balance = 23_456_782_340,
+                    AccountType = Enumerators.AccountType.Saving,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    CreatedBy = "Obinna",
+                    UpdatedBy = "Obinna"
+                }
+            };
             modelBuilder.Entity<Customer>().HasData(customers);
+            modelBuilder.Entity<Account>().HasData(accounts);
         }
 
 
