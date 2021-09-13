534cf2dbc32708ac1837e8887537f2e8440269aa
Chidiebube <notify.yourdeveloper@gmail.com>
2021-09-13 13:56:14 +0100

    some changes

---
+++ WebUI.domain/Model/EnrollCustomerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using OnlineBanking.Domain.Entities;
+using OnlineBanking.Domain.Enumerators;
+
+namespace WebUI.domain.Model
+{
+    public class EnrollCustomerViewModel
+    {
+
+        [MinLength(4), MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MinLength(4), MaxLength(50)]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public AccountType AccountType { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public Gender Gender { get; set; }
+
+    }
+}
