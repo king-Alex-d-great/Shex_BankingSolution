636a596f4736e233dc9bf29bca1dd8d49d010467
sholanejo <78520217+sholanejo@users.noreply.github.com>
2021-09-16 10:31:54 +0100

    fixed role deletion

--- WebUI.domain/Controllers/RoleController.cs
+++ WebUI.domain/Controllers/RoleController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineBanking.Domain.Entities;
-using OnlineBanking.Domain.Interfaces.Repositories;
+using OnlineBanking.Domain.Model;
+using System.Threading.Tasks;
 
 namespace WebUI.domain.Controllers
 {
@@ -20,35 +17,50 @@ namespace WebUI.domain.Controllers
             _roleManager = roleManager;
         }
 
-        public async Task <IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             var roles = await _roleManager.Roles.ToListAsync();
             return View(roles);
         }
-        public async Task <IActionResult> AddRole(string RoleName)
+        public async Task<IActionResult> AddRole(string RoleName)
         {
-            if(RoleName != null)
+            if (ModelState.IsValid)
             {
                 await _roleManager.CreateAsync(new AppRole { Name = RoleName });
             }
             return RedirectToAction("Index");
         }
-        public async Task <IActionResult> DeleteRole(string RoleName)
-        {
-                if (RoleName != null)
-                {
-                    await _roleManager.DeleteAsync(new AppRole { Name = RoleName });
-                }
-            return RedirectToAction("RemoveRole");
-        }
-        public async Task<IActionResult> UpdateRole(string RoleName)
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string RoleName)
         {
             if (RoleName != null)
             {
-                await _roleManager.UpdateAsync(new AppRole { Name = RoleName });
+                var role = await _roleManager.FindByNameAsync(RoleName);
+                await _roleManager.DeleteAsync(role);
             }
-            return RedirectToAction("UpdateRole");
+            return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public IActionResult UpdateRole()
+        {
+            return View("UpdateRole");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(string RoleName)
+        {
+            if(RoleName != null)
+            {
+                var role = await _roleManager.FindByNameAsync(RoleName);
+                var newName = new AppRole
+                {
+                    Name = role.Name
+                };
+                await _roleManager.UpdateAsync(newName);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ WebUI.domain/Model/EditRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnlineBanking.Domain.Model
+{
+    public class EditRoleViewModel
+    {
+        public string Id { get; set; }
+        [Display(Name = "Role name")]
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
